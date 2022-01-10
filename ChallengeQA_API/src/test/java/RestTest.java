7ab725acac4c709681f4c6f5a952c4a216076478
admin <tuher2u@gmail.com>
2022-01-10 12:33:12 -0300

    commit

--- ChallengeQA_API/src/test/java/RestTest.java
+++ ChallengeQA_API/src/test/java/RestTest.java
@@ -2,6 +2,8 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.path.json.JsonPath;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
@@ -17,49 +20,46 @@ public class RestTest {
 
     @Before
     public void setUp(){
-
+        //Metod el cual seta la URI
         RestAssured.baseURI = "https://api.openbrewerydb.org/";
         RestAssured.basePath= "breweries/";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 
     @Test
-    public void getListTest() {
-
-
-                given()
-                .contentType(JSON)
-                .queryParam("query", "lagunitas")
-                .get("autocomplete/")
-                .then().log().ifError()
-                .statusCode(200)
-                .body("id[0]", containsString("Lagunitas Brewing Co"));
-
-
-
-    }
-    @Test
     public void getLitstTest2(){
-
+        //Se obtiene del listado de cervecerias la cuales contienen la palabra Lagunitas
         String response = given().queryParam("query", "lagunitas")
                 .when().get("autocomplete/")
                 .then().extract().body().asString();
 
-
-        ArrayList<String> lista = from(response).get("name");
-        if(lista.contains("Lagunitas brewing co")){
+        //Se genera un arreglo donde se almacenan las cervecerias
+        ArrayList<String> lista = from(response).get("id");
+        if(lista.contains("Lagunitas Brewing Co")){
             lista.remove(2);
         }
         for (String cadena : lista){
             response = lista.toString();
         }
-        System.out.println(response.toString());
+        System.out.println(lista.toString());
+
+        String a = lista.get(0).toString();
+        String b = lista.get(1).toString();
+        System.out.println(a.toString());
+        System.out.println(b.toString());
 
 
-
-
+        //Por medio de este GET se realiza el filtrado de la cerveceria con sus respectivos assert
         String response2 = given().queryParam("query", "lagunitas")
-        .when().get(response).then().extract().body().asString();
+        .when().get(b)
+        .then().assertThat().body("state", Is.is("California"))
+                .assertThat().body("street", Is.is("1280 N McDowell Blvd"))
+                .assertThat().body("phone", Is.is("7077694495"))
+                .extract().body().asString();
+
+        System.out.println(response2);
+
+
 
 
     }
